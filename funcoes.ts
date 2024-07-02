908323fbbc4a305a781ed5f322dbdffa0d15ba7b
mauriciocampos1234 <156216039+mauriciocampos1234@users.noreply.github.com>
2024-07-02 11:41:18 -0300

    exercicio_ts

---
+++ funcoes.ts
@@ -0,0 +1,10 @@
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+}
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(multiplicacao(2, 3)); 
+console.log(saudacao("João")); 
